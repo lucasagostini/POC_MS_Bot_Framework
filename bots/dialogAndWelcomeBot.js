2974e5550f6cd877b5d56b95a0ea3b15bfbd946a
lucasagostini <lucas.agostini@compasso.com.br>
2021-11-30 09:01:24 -0300

    Criacao de arquivos, envio da mensagem inicial para novos usuarios

--- bots/dialogAndWelcomeBot.js
+++ bots/dialogAndWelcomeBot.js
@@ -1,20 +1,34 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
-const { CardFactory } = require('botbuilder');
 const { DialogBot } = require('./dialogBot');
-const WelcomeCard = require('./resources/welcomeCard.json');
+const WelcomeCard = require('./resources/welcomecard.js');
+const WELCOMED_USER = 'welcomedUserProperty';
 
 class DialogAndWelcomeBot extends DialogBot {
     constructor(conversationState, userState, dialog) {
         super(conversationState, userState, dialog);
+        this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
+
+        this.userState = userState;
 
         this.onMembersAdded(async (context, next) => {
+            const didBotWelcomedUser = await this.welcomedUserProperty.get(context, false);
+
+            // Your bot should proactively send a welcome message to a personal chat the first time
+            // (and only the first time) a user initiates a personal chat with your bot.
+            if (didBotWelcomedUser === false) {
+                // The channel should send the user name in the 'From' object
+                await context.sendActivity(WelcomeCard.messages.welcomeMessage);
+
+                // Set the flag indicating the bot handled the user's first message.
+                await this.welcomedUserProperty.set(context, true);
+            } else {
+                // to do se não for a primeira vez
+            }
             const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-                    await context.sendActivity({ attachments: [welcomeCard] });
+            for (const idx in membersAdded) {
+                if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
+                    // await context.sendActivity(WelcomeCard.messages.welcomeMessage);
                     await dialog.run(context, conversationState.createProperty('DialogState'));
                 }
             }
---
+++ bots/resources/welcomecard.js
@@ -0,0 +1,7 @@
+
+const messages = {
+    welcomeMessage: 'Oi 🙂 Eu sou o Buzz, seu parceiro virtual! \n Conte comigo para: \n🤳 Informações sobre pedidos ou chamados \n🛠️ Manutenção de equipamentos \n📑 Abrir solicitações E muito mais!'
+
+};
+
+module.exports.messages = messages;
