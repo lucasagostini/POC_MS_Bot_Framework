b630fa64805e29e96f7c1fda06a9446c427b5a33
lucasagostini <lucas.agostini@compasso.com.br>
2021-12-08 08:27:47 -0300

    adicionado number prompt pra input de dias

--- dialogs/checkDate.js
+++ dialogs/checkDate.js
@@ -1,17 +1,21 @@
 // const { LuisRecognizer, LuisBotComponent, LuisAdaptiveRecognizer } = require('botbuilder-ai');
-const { ComponentDialog, WaterfallDialog } = require('botbuilder-dialogs');
+const { ComponentDialog, WaterfallDialog, NumberPrompt } = require('botbuilder-dialogs');
 // const { isEntity } = require('botframework-schema');
 // const { DateResolverDialog } = require('./dateResolverDialog');
 const messagesPay = require('../bots/resources/messagesPay.js');
 const { ToOpenTicket } = require('./toOpenTicket.js');
+const NUMBER_PROMPT = 'numberPrompt';
 const TO_OPEN_TICKET = 'toOpenTicket';
 const CHECK_DATE = 'checkDate';
 class CheckDate extends ComponentDialog {
     constructor(id) {
         super(id || CHECK_DATE);
         this.addDialog(new ToOpenTicket(TO_OPEN_TICKET))
+            .addDialog(new NumberPrompt(NUMBER_PROMPT))
             .addDialog(new WaterfallDialog(CHECK_DATE, [
-                this.initialStep.bind(this)
+                this.initialStep.bind(this),
+                this.middleStep.bind(this),
+                this.finalStep.bind(this)
             ]));
 
         this.initialDialogId = CHECK_DATE;
@@ -19,22 +23,27 @@ class CheckDate extends ComponentDialog {
 
     async initialStep(stepContext) {
         const data = false;
-        const prazo = 9;
         if (data) {
             return stepContext.replaceDialog(TO_OPEN_TICKET);
         } else {
-            await stepContext.context.sendActivity(messagesPay.messagesFluxo.qualPrazo);
-            if (prazo >= 8) {
-                await stepContext.context.sendActivity(messagesPay.messagesFluxo.prazoInvalido);
-                if (prazo >= 8) {
-                    await stepContext.context.sendActivity(messagesPay.messagesFluxo.prazoImpossivel);
-                    return stepContext.endDialog();
-                } else {
-                    return stepContext.replaceDialog(TO_OPEN_TICKET);
-                }
-            } else {
-                return stepContext.replaceDialog(TO_OPEN_TICKET);
-            }
+            return stepContext.prompt(NUMBER_PROMPT, messagesPay.messagesFluxo.qualPrazo);
+        }
+    }
+
+    async middleStep(stepContext) {
+        if (stepContext.result >= 8) {
+            return stepContext.prompt(NUMBER_PROMPT, messagesPay.messagesFluxo.prazoInvalido);
+        } else {
+            return stepContext.replaceDialog(TO_OPEN_TICKET);
+        }
+    }
+
+    async finalStep(stepContext) {
+        if (stepContext.result >= 8) {
+            await stepContext.context.sendActivity(messagesPay.messagesFluxo.prazoImpossivel);
+            return stepContext.endDialog();
+        } else {
+            return stepContext.replaceDialog(TO_OPEN_TICKET);
         }
     }
 }
