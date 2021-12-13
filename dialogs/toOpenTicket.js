b03ce9ff303980e30845e15350e4f0aace26aaaf
lucasagostini <lucas.agostini@compasso.com.br>
2021-12-13 14:07:44 -0300

    adicionando numero do ticket na mensagem

--- dialogs/toOpenTicket.js
+++ dialogs/toOpenTicket.js
@@ -21,7 +21,7 @@ class ToOpenTicket extends ComponentDialog {
         const userData = await this.usuario.get(stepContext.context, { usuario: null });
         const ticketAdded = this.userService.addTicket(userData);
         if (ticketAdded) {
-            return stepContext.context.sendActivity(messagesPay.messagesFluxo.abriuChamado + userData.ticketNumber.toString());
+            return stepContext.context.sendActivity(messagesPay.messagesFluxo.abriuChamado + userData.ticketNumber.toString() + ' ' + messagesPay.messagesFluxo.abriuChamado2);
         } else {
             await stepContext.context.sendActivity(messagesPay.messagesFluxo.naoAbriuChamado);
         } return stepContext.endDialog();
